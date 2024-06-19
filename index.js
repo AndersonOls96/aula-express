ebef339bf2e7e63e041fd5e50999734a775fe053
Anderson <anderson.silva1996@hotmail.com>
2024-06-19 20:17:47 -0300

    criação de rotas

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const app = express()
 
+app.get("/", (req, res) => {
+    res.send('Página Inicial')
+});
+
+app.get("/sobre", (req, res) => {
+    res.send('Página Sobre')
+});
+
+app.get("/contato", (req, res) => {
+    res.send('Página Contato')
+});
+
+app.get("/produtos", (req, res) => {
+    res.send('Página Produtos')
+});
+
+app.get("/produtos/camisetas", (req, res) => {
+    res.send('Página Camisetas')
+})
+
+
 app.listen(3000, (erro) =>{
     if (erro) {
         console.log('Não foi possivel iniciar o servidor.')
